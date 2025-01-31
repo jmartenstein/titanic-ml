9a6f3f3807e7e5c349041f97f28597b9d586feb2
Justin Martenstein <jmartenstein@knockcrm.com>
2025-01-31 09:00:07 -0800

    build out hyperparameter tuning for Gradient Boosting model

--- src/analyze_titanic.py
+++ src/analyze_titanic.py
@@ -3,25 +3,31 @@ import pandas as pd
 import seaborn as sns
 import sklearn.metrics as ms
 import statistics as st
+import numpy as np
 
 from sklearn import svm, preprocessing, ensemble
-from sklearn.model_selection import train_test_split, \
+from sklearn.model_selection import TunedThresholdClassifierCV, \
+                                    RandomizedSearchCV, \
+                                    train_test_split, \
                                     KFold, \
                                     cross_val_score
 
 ### FUNCTIONS ###
 
-def print_accuracy_scores( model, X, y, num_folds ):
+def print_crossval_scores( model, X, y, num_folds ):
 
     kf = KFold(n_splits=num_folds)
 
-    mean_accuracy_scores = cross_val_score( model, X, y, cv=kf)
-    mean_accuracy1 = model.score(X, y)
-    mean_accuracy2 = st.mean(mean_accuracy_scores)
+    s_scoring = "f1"
+    l_raw_scores = cross_val_score( model, X, y, scoring=s_scoring, cv=kf)
+    l_scores = [ round(i, 2) for i in l_raw_scores ]
 
-    print(f"accuracy from score: {mean_accuracy1}")
-    print(f"acc score {num_folds} folds:   {mean_accuracy2}")
-    print(f"  each: {mean_accuracy_scores}")
+    mean_score = round(st.mean(l_scores), 3)
+    std_score = round(st.stdev(l_scores), 3)
+
+    #print(f"accuracy from score: {mean_accuracy1}")
+    print(f"{num_folds}-fold {s_scoring} mean: {mean_score}, stdev: {std_score}")
+    print(f"  each: {l_scores}")
     print()
 
     return True
@@ -64,39 +70,75 @@ def plot_roc_curve( model, X, y ):
 ### MAIN ###
 
 datestamp = "20250129.150949"
-df = pd.read_csv(f"../data/kaggle/train.clean.{datestamp}.csv")
+df_train = pd.read_csv(f"../data/kaggle/train.clean.{datestamp}.csv")
+df_test = pd.read_csv(f"../data/kaggle/test.clean.{datestamp}.csv")
 
 #x_colnames = [ "Pclass", "TitleOrd", "GroupSize", "SexOrd", "IsChild", "IsYoungChild", "Parch", "SibSp", "AgeImputed", "Fare", "FarePerPerson" ]
-x_colnames = [ "SexOrd", "FarePerPerson", "AgeImputed", "GroupSize", "Pclass" ]
+x_colnames = [ "SexOrd", "FarePerPerson", "AgeImputed" ]
 y_colname = [ "Survived" ]
 
-X = df[ x_colnames ]
-y = df[ y_colname ].values.ravel()
+X = df_train[ x_colnames ]
+y = df_train[ y_colname ].values.ravel()
 
-#corr_matrix = pd.DataFrame(X, columns=x_colnames).corr()
-#sns.heatmap(corr_matrix, annot=True, cmap='YlGnBu')
-#plt.show()
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.4)
+print("Data Shapes")
+print(f"  train: {X_train.shape}")
+print(f"  test:  {X_test.shape}")
+print()
 
+param_dist = {
+    'learning_rate': np.arange(0.01, 0.1, 0.01),
+    'n_estimators': [50, 75, 100, 125, 150, 175, 200],
+    'max_depth': [2, 3, 4, 5, 6],
+    'subsample': np.arange(0.2, 0.8, 0.1),
+    'max_features': [ 2 ],
+    'min_samples_split': [ 30 ]
+}
 
-X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.3)
-print(f"training data shape: {X_train.shape}")
-print(f"test data shape:     {X_test.shape}")
+gb_classifier = ensemble.GradientBoostingClassifier()
+random_search = RandomizedSearchCV( estimator=gb_classifier,
+                                    param_distributions=param_dist,
+                                    n_iter=50,
+                                    cv=5,
+                                    scoring='f1',
+                                    n_jobs=-1 )
 
-classifier = ensemble.GradientBoostingClassifier(n_estimators=120, max_depth=3, random_state=42)
+#classifier = ensemble.GradientBoostingClassifier(n_estimators=120, max_depth=3, random_state=42)
 #classifier = ensemble.RandomForestClassifier(n_estimators=120, random_state=42)
+#model = classifier.fit(X_train, y_train)
 
-model = classifier.fit(X_train, y_train)
+random_search.fit(X_train, y_train)
+best_params = random_search.best_params_
+model = random_search.best_estimator_
 
-print_accuracy_scores(model, X_test, y_test, 5)
+print(f"Best Params: {best_params}")
+print()
+
+print_crossval_scores(model, X_test, y_test, 5)
+
+threshold_tuner = TunedThresholdClassifierCV(
+    gb_classifier, scoring="f1", cv=5).fit(X_train, y_train)
+threshold = threshold_tuner.best_threshold_
+
+print(f"Threshold classfier score: {threshold_tuner.best_score_}")
+print()
+
+y_test_preds_proba = model.predict_proba(X_test)
+y_test_preds = (y_test_preds_proba[:,1] > threshold).astype(int)
+
+tn, fp, fn, tp = ms.confusion_matrix(y_test, y_test_preds).ravel()
+print(f"Confusion Matrix, at Threshold {threshold:.3f}")
+print(f"  tn {tn:<3}  fp {fp:<3}")
+print(f"  fn {fn:<3}  tp {tp:<3}")
+print()
+
+y_proba = model.predict_proba(df_test[x_colnames])
+y_preds = (y_proba[:,1] > threshold).astype(int)
+
+df_test["SurvivedProbability"] = y_proba[:,1]
+df_test["Survived"] = y_preds
+
 print_feature_importance(model, x_colnames)
-plot_roc_curve( model, X_test, y_test )
-
-df_test = pd.read_csv(f"../data/kaggle/test.clean.{datestamp}.csv")
-y_pred = model.predict_proba(df_test[ x_colnames ])
-df_test["SurvivedProbability"] = y_pred[:,1]
-df_test["Survived"] = (y_pred[:,1] > 0.80).astype(int)
-
-print(y_pred[-5:])
-print(df_test[["PassengerId", "Sex", "SurvivedProbability"]].tail(10))
+#plot_roc_curve( model, X_test, y_test )
 
 df_test[["PassengerId", "Survived"]].to_csv(f"../data/kaggle/submit.{datestamp}.csv", index=False)
